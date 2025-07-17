9ae46f456da9c97f12b4afe395218a9f3c6f8e82
Adam Read <aread03@qub.ac.uk>
2025-07-17 18:47:44 +0100

    Working on random data creation for later testing. Random income method created, with expense metod to be made similarly later

--- src/financeUtilities.py
+++ src/financeUtilities.py
@@ -1,5 +1,56 @@
-from financeTracker import Finances
-def loadSampleData(financeTracker):
+from datetime import datetime, timedelta
+import random
+
+income_categories = ["Pay", "Gift", "Dividend", "Loans", "Misc"]
+expense_categories = ["Bills", "Fuel", "Groceries", "Clothing", "Charity", "Emergency", "Leisure", "Misc"]
+
+def random_dates(start, end):
+    delta = end - start
+    return (start + timedelta(days = random.randint(0, delta.days))).strftime("%Y-%m-%d")
+
+def generate_student_income(n = 50):
+    transactions = []
+    start_date = datetime.now - timedelta(days = 365)
+    end_date = datetime.now()
+
+    for i in range(n):
+        category = random.choices(
+            population = income_categories,
+            weights = [50, 20, 8, 2, 20],
+            k = 1
+        )[0]
+
+        match category:
+            case "Pay":
+                amount = round(random.uniform(80, 300), 2)
+                description = "Part-time job"
+            case "Gift":
+                amount = round(random.uniform(20, 40), 2)
+                description = "Gifts from family/friends"
+            case "Dividend":
+                amount = round(random.uniform(0.1, 5), 2)
+                description = "Stock dividends"
+            case "Loans":
+                amount = round(random.uniform(1200, 2000), 2)
+                description = "Student Loans"
+            case "Misc":
+                amount = round(random.uniform(2, 20), 2)
+                description = "Miscellaneous"
+        
+        date = random_dates(start_date, end_date)
+
+        transactions.append({
+            "date": date,
+            "desctiption": description,
+            "category": category,
+            "amount": amount,
+            "type": "expense"
+        })
+
+        return transactions
+
+"""def loadSampleData(financeTracker):
     financeTracker.quickAddIncome("2025-07-03", 200, "Month's Salary", "Pay")
     financeTracker.quickAddExpense("2025-07-04", 50, "Electric Bill", "Bills")
     financeTracker.quickAddExpense("2025-07-05", 20, "Bowling", "Leisure")
+"""
--- src/main.py
+++ src/main.py
@@ -3,10 +3,3 @@ import financeUtilities
 
 mgr = manager.Manager()
 mgr.test()
-"""mgr.financeTracker.addIncome()
-mgr.financeTracker.addExpense()
-mgr.financeTracker.saveAllTransactions()"""
-mgr.financeTracker.clearTransactions()
-financeUtilities.loadSampleData(mgr.financeTracker)
-mgr.financeTracker.listTransactions()
-mgr.financeTracker.saveAllTransactions()
