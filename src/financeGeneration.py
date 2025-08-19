cbd6078c30429062958c1e6842019b3013898672
Adam Read <aread03@qub.ac.uk>
2025-08-19 18:52:06 +0100

    Expanded the CSVStatementConverter function, and created a category map for later implementation in said method

--- src/financeGeneration.py
+++ src/financeGeneration.py
@@ -6,6 +6,39 @@ from transaction import Transaction
 income_categories = ["Pay", "Gift", "Dividend", "Loans", "Misc"]
 expense_categories = ["Bills", "Fuel", "Groceries", "Clothing", "Charity", "Emergency", "Leisure", "Misc"]
 
+CATEGORY_MAP = {("Salary and pension", "Salary / wages"): "Pay",
+        ("Other income", "Own account transfer"): "Loans",    # assuming transfers are loans
+        ("Other income", "Other transfers"): "Gift",
+        
+        # Expenses
+        ("Transport", "Fuel"): "Fuel",
+        ("Transport", "Bus / train"): "Bills",   # regular travel → treat as bills
+        ("Transport", "Plane"): "Leisure",       # flights = holiday/leisure
+        ("Transport", "Parking"): "Bills",
+
+        ("Household goods", "Supermarket"): "Groceries",
+        ("Household goods", "Other"): "Groceries",   # general shops
+        ("Household goods", None): "Groceries",
+
+        ("Recreation and leisure", "Caf� / restaurant"): "Leisure",
+        ("Recreation and leisure", "Bar / nightclub"): "Leisure",
+        ("Recreation and leisure", "Cinema / concert / theatre"): "Leisure",
+        ("Recreation and leisure", "Holiday"): "Leisure",
+        ("Recreation and leisure", "Games / toys"): "Leisure",
+
+        ("Other expenses", "Donations"): "Charity",
+        ("Other expenses", "Cash withdrawals"): "Misc",       # ATM cash → misc
+        ("Other expenses", "Own account transfer"): "Misc",   # treat refund/overpay → misc
+
+        ("Clothing, shoes and personal care", "Clothing / shoes"): "Clothing",
+        ("Clothing, shoes and personal care", "Personal care"): "Clothing", # lumped
+
+        ("Housing", "Maintenance"): "Bills",
+        ("Housing", "Other"): "Bills",     # e.g. Belfast Cathedral En
+
+        ("Uncategorised", "Uncategorised"): "Misc",
+    }
+
 def random_dates(start, end):
     delta = end - start
     return (start + timedelta(days = random.randint(0, delta.days))).strftime("%Y-%m-%d")
@@ -103,18 +136,43 @@ def generate_student_expenses(n = 150):
     
     return transactions
 
-#to do: generate method to translate actual bank statement csv
-#read the file
+
 def CSVStatementConverter(filename = "Statement Jan-Jun 25.csv", output = "statement.csv"):
+
     output_rows = []
 
     with open(file = filename, mode = "r", encoding = "cp1252", newline = "") as f:
         reader = csv.DictReader(f)
+        print(reader.fieldnames)
+
         for line in reader:
             #remove unecessary categories
             line.pop("Reconciled", None)
             line.pop("Status", None)
             line.pop("Balance", None)
+
+            #create "desc" attribute
+            line["desc"] = line.pop("Text")
+
+            #create is_income attribute for each row
+            amount_str = line["Amount"].replace(",", "").strip()
+            line["is_income"] = float(amount_str) > 0
+            line["Amount"] = amount_str
+
+            #adjust categories to match above categories
+            
+
+            #djust dates into YYYY/MM/DD format
+
+            #create transaction objects for each line
+            new_trans = Transaction(
+                category = line["Category"], 
+                date = line["date"], 
+                amount = line["Amount"], 
+                desc = line["desc"], 
+                is_income = line["is_income"]
+            )
+
             output_rows.append(line)
     return output_rows
 
