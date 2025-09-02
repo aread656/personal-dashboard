4324ecfe45ec24bce9dba163a468f9d60068a462
Adam Read <aread03@qub.ac.uk>
2025-09-02 13:17:40 +0100

    Fleshed out income/expensesByDates functions in financeStatistics. Added helper methods to reduce duplication, began working on a netIncomePlotting method with similar functionality

--- src/financeStatistics.py
+++ src/financeStatistics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import seaborn as sns
 import pandas as pd
-from matplotlib import pyplot as py
+from matplotlib import pyplot as plt
 
 """statistics to be made:
     net income
@@ -14,21 +14,47 @@ def totalExpenses(fin) -> float:
     return sum(t.amount for t in fin.getAllTransactions() if not t.is_income and t.amount < 1000)
 def netIncome(fin) -> float:
     return totalIncome(fin) - totalExpenses(fin)
-def incomeByDates(fin):
-    #plot a histogram of income by month
-    #create a dataset with dates and amounts, filtered by is_income
-    #dataset is taken from fin(getAllTransactions), returns fin.transactions
-    full_dataset = fin.getAllTransactions()
-    filtered_records = pd.DataFrame(trans.__dict__ for trans in full_dataset if trans.is_income)[["date", "amount"]]
-    filtered_records["month_name"] = filtered_records["date"].dt.strftime("%m")
-    #group by month and sum amounts
-    monthly_sums = filtered_records.groupby("month_name")["amount"].sum()
-    #sort by calendar order
 
-    return filtered_records
+
+def datesDataset(fin, income_true_false):
+    full_dataset = fin.getAllTransactions()
+    filtered_dataset = pd.DataFrame(
+        {"date": trans.date,
+         "amount": trans.amount}
+        for trans in full_dataset if trans.is_income == income_true_false)
+    return filtered_dataset
+def incomeByDates(fin):
+    income_dates_dataset = datesDataset(fin, True)
+    income_dates_dataset["month"] = income_dates_dataset["date"].dt.month
+    income_dates_dataset["month_name"] = income_dates_dataset["date"].dt.strftime("%m")
+    monthly_sums = income_dates_dataset.groupby(["month", "month_name"])["amount"].sum().reset_index().sort_values("month")
+
+    sns.barplot(data = monthly_sums, x = "month_name", y = "amount",
+                order = monthly_sums["month_name"])
+    plt.title("Income by Month")
+    plt.xlabel("Month")
+    plt.ylabel("Amount")
+    plt.show()
+    return
 def expensesByDates(fin):
+    expenses_dates_dataset = datesDataset(fin, False)
+    expenses_dates_dataset["month"] = expenses_dates_dataset["date"].dt.month
+    expenses_dates_dataset["month_name"] = expenses_dates_dataset["date"].dt.strftime("%m")
+    monthly_sums = expenses_dates_dataset.groupby(["month", "month_name"])["amount"].sum().reset_index().sort_values("month")
+    print(monthly_sums)
+
+    sns.barplot(data = monthly_sums, x = "month_name", y = "amount")
+    plt.title("Expenses by month")
+    plt.xlabel("Month")
+    plt.ylabel("Amount")
+    plt.show()
     return
 def netIncomeByDates(fin):
+    income_dataset = datesDataset(fin, True)
+    expenses_dataset = datesDataset(fin, False)
+    #create a net dataset from each dataset's sums
     return
+
+
 def mostCommonCategories(fin):
     return
--- src/main.py
+++ src/main.py
@@ -4,12 +4,5 @@ import financeStatistics as fs
 
 mgr = manager.Manager()
 finances = mgr.financeTracker
-finances.clearTransactions()
-finances.transactions.extend(fg.CSVStatementConverter(filename = "Statement Jan-Jun 25.csv"))
-#finances.printAllTransactions()
-finances.saveAllTransactions()
-"""print(fs.totalIncome(finances))
-print(fs.totalExpenses(finances))
-print(fs.netIncome(finances))
-print(finances.getAllTransactions())"""
-print(fs.incomeByDates(finances))
+fs.incomeByDates(finances)
+fs.expensesByDates(finances)
