dd6d8062d8792996a2e25de24ee1aec3aa81f81d
Adam Read <aread03@qub.ac.uk>
2025-10-14 00:21:28 +0100

    Expanded Menu class functionality, removed unneeded main.py comments from old functionality.

--- src/main.py
+++ src/main.py
@@ -1,37 +1,7 @@
-import financeGeneration as fg
-import financeStatistics as fs
-import personalFunctions as pf
 from financeTracker import Finances
 from menu import Menu
 
 finances = Finances()
 
-"""finances.clearTransactions()
-
-finances.transactions = fg.CSVStatementConverter("Statement Jan-Jun 25.csv")
-finances.printAllTransactions()
-finances.saveAllTransactions()
-
-finances.clearTransactions()
-finances.transactions.extend(fg.generate_student_income())
-finances.transactions.extend(fg.generate_student_expenses())
-finances.printAllTransactions()
-finances.saveAllTransactions()
-
-pf.removeUnwantedRows(finances)
-fs.printAllIncomesOrExpenses(finances, True)
-print("\n\n")
-fs.printAllIncomesOrExpenses(finances, False)
-
-fs.incomeByDates(finances)
-fs.expensesByDates(finances)
-fs.netIncomeByDates(finances)
-
-fs.mostCommonCategories(finances)
-fs.amountsByCategory(finances)
-
-print(fs.filterRecordsByDates(finances, "2025-03-01", "2025-03-31"))
-"""
-
 m = Menu()
-m.displayMainMenu()
+m.mainMenu()
--- src/menu.py
+++ src/menu.py
@@ -5,62 +5,96 @@ class Menu:
     def __init__(self):
         self.f = Finances()
 
-    def displayMainMenu(self):
-        OPTIONS_MAP = {
-            1:self.viewTransactions,
-            2:lambda: print("Stats Menu"),
-            3:lambda: print("Modification Menu")
-        }
-        cont = True
-        print("Welcome to the Finance Tracker!")
-        while(cont):
-            print("Options:")
-            print("1: View Transactions\n2: Statistics\n3: Modify Transactions")
-            option = self.getUserOption(3)
-            func = OPTIONS_MAP.get(option)
-            if func:
-                func()
-            else:
-                continue
-            """match (option):
+    def mainMenu(self):
+        print("\nWelcome to the Finance Tracker!")
+        while(True):
+            print("Main Menu:")
+            print("1. View Transactions\n2. Statistics\n3. Modify Transactions\n4. Quit")
+            option = self.getUserOption(4)
+            match (option):
                 case 1:
                     self.viewTransactions()
                     cont = False
-                    return
                 case 2:
-                    self.displayMainMenu()
+                    self.statsMenu()
                 case 3:
-                    self.displayMainMenu()"""
-            #1: view all transactions
-            #2: statistics
-            #3: modify transactions
-        return
+                    self.modifyMenu()
+                case 4:
+                    print("Goodbye!")
+                    return
     def getUserOption(self, upper_limit) -> int:
-        cont = True
-        try:
-            option = int(input(f"Enter your option: 1 - {upper_limit}: "))
-            if (1 <= option <= upper_limit):
-                print("Yes")
-                cont = False
-            else:
-                print("Invalid option. Try again.")
-        except Exception:
-            print("Invalid input. Try again")
-        return option
+        while(True):
+            try:
+                option = int(input(f"Enter your option: 1 - {upper_limit}: "))
+                if (1 <= option <= upper_limit):
+                    return option
+                else:
+                    print("Invalid option. Try again.")
+            except Exception:
+                print("Invalid input. Try again")
     
     def viewTransactions(self):
-        OPTIONS_MAP = {
-            "1" : lambda: self.f.listTransactions(),
-            "2" : lambda: fs.printAllIncomesOrExpenses(self.f, True),
-            "3" : lambda: fs.printAllIncomesOrExpenses(self.f, False),
-            "4" : lambda: print("Under construction"),
-            "5" : lambda: self.displayMainMenu()
-        }
-        cont = True
         print("View Transactions Menu")
-        while(cont):
+        while(True):
             print("1. All Transactions\n2: All Income\n3. All Expenses\n4: Filter Transactions by Dates\n5: Main Menu")
             option = self.getUserOption(5)
-            func = OPTIONS_MAP.get(option)
-            if func:
-                func()
+            match(option):
+                case 1:
+                    self.f.listTransactions()
+                case 2:
+                    fs.printAllIncomesOrExpenses(self.f, True)
+                case 3:
+                    fs.printAllIncomesOrExpenses(self.f, False)
+                case 4:
+                    print("Under construction")
+                case 5:
+                    return
+
+    def statsMenu(self):
+        print("Statistics Menu")
+        while(True):
+            print("1. Date Functions\n2. Category Functions" \
+            "\n3. Misc Functions\n4. Main Menu")
+            option = self.getUserOption(4)
+            match(option):
+                case 1:
+                    self.dateStatsMenu()
+                case 2:
+                    self.categoryStatsMenu()
+                case 3:
+                    self.miscStatsMenu()
+                case 4:
+                    return
+    def dateStatsMenu(self):
+        print("\nDates menu")
+        print("1. Filter Records by Dates\n2. Income by Dates\n3. Expenses by Dates" \
+        "\n4. Net Income by Dates\n5. Stats Menu")
+        option = self.getUserOption(5)
+        match(option):
+            case 1:
+                print("Under construction")
+            case 2:
+                fs.incomeByDates(self.f)
+            case 3:
+                fs.expensesByDates(self.f)
+            case 4:
+                fs.netIncomeByDates(self.f)
+            case 5:
+                return
+    def categoryStatsMenu(self):
+        print("Category Menu")
+        print("1. Most Common Categories\n2. Amounts by Category\n3. Stats Menu")
+        option = self.getUserOption(3)
+        match(option):
+            case 1:
+                fs.mostCommonCategories(self.f)
+            case 2:
+                fs.amountsByCategory(self.f)
+            case 3:
+                return
+    def miscStatsMenu(self):
+        print("Misc menu")
+        return
+
+    def modifyMenu(self):
+        return
