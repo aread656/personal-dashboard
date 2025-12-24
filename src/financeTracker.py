826a97b072c8ddd8b5d4f2cac68c558ac825e338
Adam Read <aread03@qub.ac.uk>
2025-12-24 01:12:57 +0000

    Removed old functions, corrected editing logic

--- src/financeTracker.py
+++ src/financeTracker.py
@@ -43,48 +43,15 @@ class Finances:
             confirm = input("Confirm transaction details? (Y/N): ").strip().upper()
             if (confirm == "Y"):
                 #addIncome
-                return new_trans
+                self.transactions.append(new_trans)
+                self.saveAllTransactions()
+                return
             else:
                 print("Transaction cancelled")
                 return None
         except Exception as e:
                 print(f"An error occurred: {e}")
-    def addIncome(self):
-        print("Adding income...")
-        income = self.create_transaction(self.income_categories, True)
-        if income:
-            self.transactions.append(income)
-            self.addTransactionCSV(income)
-            print(f"Income added! ID:{income.id}")
-    def addExpense(self):
-        print("Adding an expense...")
-        expense = self.create_transaction(self.expense_categories, False)
-        if expense:
-            self.transactions.append(expense)
-            self.addTransactionCSV(expense)
-            print(f"Expense added! ID:{expense.id}")
-
-    #----------quickAdd methods----------#
-    def quickAdd(self, category, date, amount, desc, is_income):
-        #quick add transaction details
-        try:
-            #creates a date object under the format of DD/MM/YYYY
-            date_obj = datetime.strptime(date, "%Y-%m-%d")
-            #create a new transaction with details
-            new_trans = Transaction(category, date_obj, amount, desc, is_income)
-            self.transactions.append(new_trans)
-            self.addTransactionCSV(new_trans)
-            print(f"{'Income' if is_income else 'Expense'} added! ID: {new_trans.id}")
-        except ValueError:
-            print("Invalid date format. Please use YYYY-MM-DD")
-    def quickAddIncome(self, date, amount, desc, category = "Misc"):
-        #quick add for income, no prompt
-        self.quickAdd(category, date, amount, desc, True)
-    def quickAddExpense(self, date, amount, desc, category = "Misc"):
-        #quick add for expense, no prompt
-        self.quickAdd(category, date, amount, desc, False)
     
-
     #----------listing and searching----------#
     def listTransactions(self):
         #lists all transactions
@@ -120,7 +87,7 @@ class Finances:
             print(f"Transaction with id {trans_id} not found"); return
         new_amount = new_desc = new_date = new_cat = new_type = None
 
-        attribute = input("Enter the attribute to edit (date, category, type, amount, or desc): ")
+        attribute = input("Enter the attribute to edit (date, category, type, amount, or desc): ").strip().lower()
         match(attribute):
             case "date":
                 new_date = Transaction.transaction_date(self)
@@ -132,32 +99,30 @@ class Finances:
                 new_amount = Transaction.transaction_amount(self)
             case "desc":
                 new_desc = Transaction.transaction_desc(self)
-            case "_":
+            case _:
                 print("Invalid. Please try again"); return
 
-        if new_amount is not None: t.amount = print(f"New amount: {new_amount}") 
-        if new_date is not None: t.date = print(f"New date: {new_date}") 
-        if new_cat is not None: t.category = print(f"New category: {new_cat}") 
-        if new_type is not None: t.is_income = print(f"New type: {new_type}") 
-        if new_desc is not None: t.desc = print(f"New desc: {new_desc}")  
+        if new_amount is not None: print(f"New amount: {new_amount}") 
+        if new_date is not None: print(f"New date: {new_date}") 
+        if new_cat is not None: print(f"New category: {new_cat}") 
+        if new_type is not None: print(f"New type: {new_type}") 
+        if new_desc is not None: print(f"New desc: {new_desc}")  
 
         confirm = input("Confirm edit? (Y/N): ").strip().upper()
         if(confirm == "Y"):
-            if new_amount is not None: t.amount = new_amount 
-            if new_date is not None: t.date = new_date
-            if new_cat is not None: t.category = new_cat
-            if new_type is not None: t.is_income = new_type
-            if new_desc is not None: t.desc = new_desc 
+            if new_amount is not None: trans.amount = new_amount 
+            if new_date is not None: trans.date = new_date
+            if new_cat is not None: trans.category = new_cat
+            if new_type is not None: trans.is_income = new_type
+            if new_desc is not None: trans.desc = new_desc 
             self.saveAllTransactions()
             print("Edits made and saved")
         else:
             print("No edits made")
-        
-
-    def deleteTransaction(self, trans_id = None, trans_amount = None, trans_desc = None):
+    def deleteTransaction(self, trans_id = None):
         #deletes a transaction based on ID
         for t in self.transactions:
-            if t.id == trans_id or t.amount == trans_amount or t.desc == trans_desc:
+            if t.id == trans_id:
                 try:
                     print(f"\nFound transaction:\n{t}")
                     confirm = input(f"Delete transaction? (Y/N): ").strip().upper()
@@ -174,21 +139,6 @@ class Finances:
         print(f"Transaction with {trans_id} not found")
 
     #----------CSV management----------#
-    def addTransactionCSV(self, transaction):
-        #adds a transaction to the CSV file
-        file_exists = os.path.isfile(self.filename) and os.path.getsize(self.filename) > 0 
-        with open(self.filename, mode = "a", newline = "", encoding = "utf-8") as f:
-            writer = csv.writer(f)
-            if not file_exists:
-                writer.writerow(["id", "type", "category", "date", "amount", "desc"])
-            writer.writerow([
-                transaction.id,
-                "income" if transaction.is_income else "expense",
-                transaction.category,
-                transaction.date.strftime("%Y-%m-%d"),
-                transaction.amount,
-                transaction.desc
-                ])
     def loadTransactions(self):
         #load all transactions into self.transactions
         file_exists = os.path.isfile(self.filename) and os.path.getsize(self.filename) > 0
