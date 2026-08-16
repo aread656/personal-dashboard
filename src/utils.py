4546056878dfaaa1e8d94e7084dff7f9f55f4513
Adam Read <aread03@qub.ac.uk>
2026-08-16 21:12:49 +0100

    Refactoring constants (moved to constants.py)

--- src/utils.py
+++ src/utils.py
@@ -3,45 +3,6 @@ import random
 from transaction import Transaction
 from finance import Finance
 
-INCOME_CATEGORIES = ["Pay", "Gift", "Dividend", "Loans", "Misc"]
-EXPENSE_CATEGORIES = ["Bills", "Transport", "Groceries", "Clothing", "Charity", "Emergency", "Leisure", "Misc"]
-
-CATEGORY_MAP = {
-    ("Salary and pension", "Salary / wages"): "Pay",
-    ("Salary and pension","Student loan"):"Loans",
-    ("Pension, savings and investment","Savings"):"Savings",
-
-    ("Household goods","Supermarket"):"Groceries",
-    ("Household goods","Other"):"Groceries",
-
-    ("Transport", "Fuel"): "Transport",
-    ("Transport", "Bus / train"): "Transport",
-    ("Transport", "Plane"): "Leisure",
-    ("Transport", "Parking"): "Transport",
-    
-    ("Recreation and leisure", "Caf� / restaurant"): "Leisure",
-    ("Recreation and leisure", "Bar / nightclub"): "Leisure",
-    ("Recreation and leisure", "Cinema / concert / theatre"): "Leisure",
-    ("Recreation and leisure", "Holiday"): "Leisure",
-    ("Recreation and leisure", "Games / toys"): "Leisure",
-    
-    ("Uncategorised","Uncategorised"):"Misc",
-    
-    ("Recreation and leisure","Other"):"Leisure",
-    
-    ("Housing", "Maintenance"): "Bills",
-    ("Housing", "Other"): "Bills",
-
-    ("Clothing, shoes and personal care", "Clothing / shoes"): "Clothing",
-    ("Clothing, shoes and personal care", "Personal care"): "Clothing",
-
-    ("Other income","Own account transfer"):"Transfer",
-    ("Other income","Other transfers"):"Misc",
-    ("Other expenses","Fees"):"Misc",
-    ("Other expenses", "Donations"): "Charity",
-    ("Other expenses", "Cash withdrawals"): "Misc",
-}
-
 def random_dates(start, end):
     delta = end - start
     return (start + timedelta(days = random.randint(0, delta.days))).strftime("%Y-%m-%d")
