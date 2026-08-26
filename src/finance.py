3896bed8bb111eab204b45c2475e89b7b43e994c
Adam Read <aread03@qub.ac.uk>
2026-08-26 11:33:57 +0100

    findTransaction and deleteTransaction robustness improved

--- src/finance.py
+++ src/finance.py
@@ -71,7 +71,7 @@ class Finance:
             return None
 
     def deleteTransaction(self,id) -> Transaction | None:
-        if (id is None) or (id.lower() == "q" and isinstance(id,str)): return None
+        if (id is None) or (not isinstance(id,str)) or (id.lower() == "q"): return None
         for i,t in enumerate(self.transactions):
             if t.id == id:
                 return self.transactions.pop(i)
@@ -115,7 +115,7 @@ class Finance:
             return False
 
     def findTransaction(self,id):
-        if (id.lower() == "q" and isinstance(id,str)): return None
+        if (id is None) or (not isinstance(id,str)) or (id.lower() == "q"): return None
         for t in self.transactions:
             if t.id == id:
                 return t
